8498dc823fa0e535b203a898ba1cceffa2947ff2
Aman Vishwakarma <143378283+its-Aman007@users.noreply.github.com>
2025-02-28 00:45:14 +0530

    Add files via upload

---
+++ 1_basics/num_math.js
@@ -0,0 +1,21 @@
+const score=100
+console.log(score)
+const bonus=new Number(50)
+console.log(bonus)
+
+console.log(bonus.toString().length)
+
+
+///////////////////////////////////// Math //////////////////////////
+
+console.log(Math.abs(-5))
+console.log(Math.ceil(5.1))
+console.log(Math.floor(5.9))
+console.log(Math.round(5.5));
+
+
+const max=15
+const min=10
+console.log(Math.floor(Math.random()*(max-min))+min)
+
+
