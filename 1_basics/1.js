ef49054b0b0a96096e9aa4fce4178014c2dd7a73
Aman Vishwakarma <143378283+its-Aman007@users.noreply.github.com>
2025-03-05 00:42:32 +0530

    Add files via upload

---
+++ 1_basics/1.js
@@ -0,0 +1,39 @@
+/// For Loop
+
+for(let index=0; index<=10;index++){
+    const element=index;
+    console.log(element);
+}
+
+
+
+
+for(let i=0;i<=10;i++){
+    console.log(i)
+    for(let j=0;j<=10;j++){
+        console.log(i+'*'+j+'='+i*j);
+    }
+}
+
+
+
+
+let myarray=['Batman','Superman','Flash']
+console.log(myarray.length)
+for(let i=0;i<myarray.length;i++){
+    
+    console.log(myarray[i])
+}
+
+
+
+
+
+let newarray=['Batman','Superman','Flash']
+console.log(newarray.length)
+for(let i=0;i<myarray.length;i++){
+    if(i==2){
+        break; // This will break the loop when i==2
+    }
+    console.log(newarray[i])
+}
